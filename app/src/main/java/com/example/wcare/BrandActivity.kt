ee6596bd01a8fd01cd0a89f3cbbe04f3856dcd66
ashrvarr <rabinaashirova@mail.ru>
2025-02-19 00:07:24 +0500

    Initial commit

---
+++ app/src/main/java/com/example/wcare/BrandActivity.kt
@@ -0,0 +1,34 @@
+package com.example.wcare
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BrandActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_brand)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // here setKeepOnScreenCondition false so, activity redirect another activity
+        // and some api call here
+        // if setKeepOnScreenCondition true so, activity code not redirect another activity
+        splashScreen.setKeepOnScreenCondition { true }
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
