53444e4553d2d31f06612eb2bb5a37bce6bab909
апукачи <210103415@stu.sdu.edu.kz>
2025-02-19 17:23:27 +0600

    loading_dialog page
    need signupbutton in register acitivty

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("com.airbnb.android:lottie:6.6.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/wcare/RegisterActivity.kt
+++ app/src/main/java/com/example/wcare/RegisterActivity.kt
@@ -1,7 +1,9 @@
 package com.example.wcare
 
+import android.app.Dialog
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,14 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        val loadingDialog = Dialog(this, R.style.DialogCustomeTheme).apply {
+            setContentView(R.layout.loading_dialog)
+            window!!.setLayout(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            setCancelable(false)
+        }
         val logInBtn = findViewById<Button>(R.id.logInBtn)
         logInBtn.setOnClickListener{
             finish()
