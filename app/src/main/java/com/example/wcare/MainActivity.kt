1cffb6e7656e37834d1970f5b7b504a16a32fbd5
апукачи <210103415@stu.sdu.edu.kz>
2025-02-19 21:55:13 +0600

    Log Out function

--- app/src/main/java/com/example/wcare/MainActivity.kt
+++ app/src/main/java/com/example/wcare/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.wcare
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -21,13 +23,25 @@ class MainActivity : AppCompatActivity() {
 
         val displayTxt = findViewById<TextView>(R.id.displayTxt)
 
+        //Checking fetched data after log in
         val currentUser = FirebaseAuth.getInstance().currentUser
-
         if (currentUser != null){
             val email = currentUser.email
             val userName = currentUser.displayName
 
             displayTxt.text = ("Email = ${email}\nUsername = ${userName}")
         }
+
+        val logoutBtn = findViewById<Button>(R.id.logoutBtn)
+        logoutBtn.setOnClickListener {
+            // Logging Out tf
+            FirebaseAuth.getInstance().signOut()
+            val mainIntent = Intent(this,LoginActivity::class.java)
+            mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(mainIntent)
+            finish()
+        }
+
     }
 }
